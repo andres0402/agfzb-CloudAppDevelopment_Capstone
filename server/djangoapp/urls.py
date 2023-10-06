7d8fb2d0b30169fa17e2a505fb5bf4079a73468e
Your Name <you@example.com>
2023-10-06 14:10:05 -0400

    login signup

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,9 +18,15 @@ urlpatterns = [
 
     # path for registration
 
+    path(route='signup', view=views.registration_request, name='signup'),
+
     # path for login
 
+    path(route='login', view=views.login_request, name='login'),
+
     # path for logout
+
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index')
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,16 +30,41 @@ def contact(request):
     return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return HttpResponse('Invalid credentials')
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect("djangoapp:index")
+
+
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        password = request.POST['password']
+        
+        user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')  
+        else:
+            return HttpResponse('Error')
+
+    return render(request, 'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
